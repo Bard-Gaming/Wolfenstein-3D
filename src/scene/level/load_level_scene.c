678359da59d4829b4a26af48ef37dbabbafeb523
Bard-Gaming <christophe@dronne.fr>
2025-05-24 18:19:42 +0200

    [src/scene] Updated load scene to place the player in the starting room

--- src/scene/level/load_level_scene.c
+++ src/scene/level/load_level_scene.c
@@ -14,7 +14,13 @@
 static void load_textures(void)
 {
     crpt_load_texture(SOLDIER_ASSET("idle_front"), "soldier_idle_front");
+    crpt_load_texture("assets/textures/rocks.png", "rocks");
     crpt_load_texture("assets/textures/blue_wall.png", "blue_wall");
+    crpt_load_texture("assets/textures/stone_wall.png", "stone_wall");
+    crpt_load_texture("assets/textures/brick_wall.png", "brick_wall");
+    crpt_load_texture("assets/textures/plank_wall.png", "plank_wall");
+    crpt_load_texture("assets/textures/mossy_stone_wall.png",
+        "mossy_stone_wall");
     crpt_load_texture("assets/sprites/player/healthbar_full.png", "hb_full");
     crpt_load_texture("assets/sprites/player/healthbar_empty.png", "hb_empty");
 }
@@ -25,6 +31,15 @@ static void load_enemies(map_t *map)
     crpt_map_add_object(map, (void *)create_soldier((vec2_t){ 342.0, 513.0 }));
     crpt_map_add_object(map, (void *)create_soldier((vec2_t){ 720.0, 78.0 }));
     crpt_map_add_object(map, (void *)create_soldier((vec2_t){ 563.0, 391.0 }));
+}
+
+static void place_player(void)
+{
+    player_t *player = get_player();
+
+    player->pos->x = 4512.0;
+    player->pos->y = 4256.0;
+    crpt_camera_set_rotation(M_PI_2);
 }
 
 /*
@@ -40,4 +55,5 @@ void load_level_scene(scene_t *scene)
     scene->map->ceiling = (color_t){ 56, 56, 56, 255 };
     load_enemies(scene->map);
     init_player();
+    place_player();
 }
